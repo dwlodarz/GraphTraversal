9300e6c3262e7d447c25dccafea8712d15bacfd3
Dominik <dominik.wlodarz@gmail.com>
2016-10-24 18:01:00 +0200

    Reorganised the WebServices projects. Moved Interfaces (contracts) into its own directory.

--- GraphTraversal.WebServices/Contracts/IDataManagementService.cs
+++ GraphTraversal.WebServices/Contracts/IDataManagementService.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace GraphTraversal.WebServices
+namespace GraphTraversal.WebServices.Contracts
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDataManagementService" in both code and config file together.
     [ServiceContract]
