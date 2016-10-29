ed965957fa616a96dcfc3c48bbeaf51e89e8cf7b
Dominik <dominik.wlodarz@gmail.com>
2016-10-29 20:13:48 +0200

    Unit tests fixed and changed the version on NUnit

--- GraphTraversal.Business.Tests/NodeManagerTests.cs
+++ GraphTraversal.Business.Tests/NodeManagerTests.cs
@@ -48,18 +48,19 @@ namespace GraphTraversal.Business.Tests
         public async Task RelatePath_SuccesfullyCallsAddAndRelate()
         {
             var nodeModel = this.fixture.Build<NodeModel>().Without(x => x.AdjacentNodes).CreateAnonymous();
-            var adjacentNodes = this.fixture.CreateMany<NodeEntity>(2);
+            var adjacentNodes = this.fixture.Build<NodeModel>().Without(x => x.AdjacentNodes).CreateMany(2);
+            nodeModel.AdjacentNodes = adjacentNodes.ToList() ;
+
             var nodeEntity = new NodeEntity { Id = nodeModel.Id, Label = nodeModel.Label, Cost = 1 };
 
 
             this.repository.AddOrUpdateAsync(Arg.Any<NodeEntity>()).Returns(Task.FromResult<NodeEntity>(nodeEntity));
-            this.repository.AdjacentNodesIfDoNotExistAsync(nodeModel.Id, adjacentNodes).Returns(Task.FromResult(0));
+            this.repository.AdjacentNodesIfDoNotExistAsync(Arg.Any<string>(), Arg.Any<IEnumerable<NodeEntity>>()).Returns(Task.FromResult(0));
 
             await this.nodeManager.RelatePath(nodeModel);
 
-            //calculator.Received().Add(Arg.Any<int>(), 2);
             await this.repository.Received(1).AddOrUpdateAsync(Arg.Is<NodeEntity>(x => x.Id == nodeEntity.Id));
-            await this.repository.Received(1).AdjacentNodesIfDoNotExistAsync(Arg.Is<string>(x => x == nodeEntity.Id), Arg.Is<IEnumerable<NodeEntity>>(a => a.Count() == adjacentNodes.Count()));
+            await this.repository.Received(1).AdjacentNodesIfDoNotExistAsync(Arg.Is<string>(x => x == nodeEntity.Id), Arg.Is<IList<NodeEntity>>(a => a.Count() == adjacentNodes.Count()));
         }
     }
 }
